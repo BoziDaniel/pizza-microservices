6045bd7c114fb47784cb3e39595a952d8d015cb2
BoziDaniel <daniel.bozi88@gmail.com>
2020-06-16 14:16:52 +0200

    implemented get methods in UserController

---
+++ user-service/src/main/java/com/codecool/userservice/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.codecool.userservice.controller;
+
+import com.codecool.userservice.entity.User;
+import com.codecool.userservice.modell.UserRole;
+import com.codecool.userservice.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User getUserByUserId(@PathVariable("id") Long id) {
+        log.info(String.format("request arrived at '/user/%s'", id));
+        User user = userRepository.getUserById(id);
+        log.info(String.format("request processed '/user/%s' , found user: %s", id , user.toString()));
+        return user;
+    }
+
+    @GetMapping("/{id}/role")
+    public UserRole getUserRoleById(@PathVariable("id") Long id) {
+        log.info(String.format("request arrived at '/user/%s/role'", id));
+        UserRole role=  userRepository.getUserRoleByUserId(id);
+        log.info(String.format("request processed '/user/%s/role' , found  role: %s", id , role.toString()));
+        return role;
+    }
+//itt usercredentialt kéne visszaadni!!! majd átír
+    @GetMapping
+    public User getUserCredentialsByUsername(@RequestParam("username") String username) {
+        log.info(String.format("request arrived at /user?username=%s", username  ));
+        User user = userRepository.getUserByUsername(username);
+        log.info(String.format("request processed /user?username=%s found user: %s", username, user.toString()));
+        return user;
+    }
+}
--- user-service/src/main/java/com/codecool/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/codecool/userservice/repository/UserRepository.java
@@ -1,16 +1,19 @@
 package com.codecool.userservice.repository;
 
 import com.codecool.userservice.entity.User;
+import com.codecool.userservice.modell.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User getUserById(Long Id);
 
     @Query(value = "SELECT ROLE FROM USER WHERE ID=?1", nativeQuery = true)
-    String getUserRoleByUserId(Long userId);
+    UserRole getUserRoleByUserId(Long userId);
 
-
+    User getUserByUsername(String userName);
 //    Optional<UserCredential> getUserByUsername(String userName);
 
 }
